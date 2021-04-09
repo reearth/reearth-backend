74c4e85614e2a4cf6609ee03676e20e74cc0e952
Ya Ka <eng.ymk@gmail.com>
2021-04-09 08:51:35 +0300

    testing pkg/id/plugin.go

--- pkg/id/plugin_test.go
+++ pkg/id/plugin_test.go
@@ -12,13 +12,99 @@ var _ encoding.TextMarshaler = (*PluginID)(nil)
 var _ encoding.TextUnmarshaler = (*PluginID)(nil)
 
 func TestPluginIDValidator(t *testing.T) {
-	assert.True(t, validatePluginName("1cc.1_c-d"), "1cc.1_c-d")
-	assert.True(t, validatePluginName(strings.Repeat("a", 100)), "100 chars")
-	assert.False(t, validatePluginName(""), "empty")
-	assert.False(t, validatePluginName("  "), "space")
-	assert.False(t, validatePluginName("@bbb/aa-a_a"), "@bbb/aa-a_a")
-	assert.False(t, validatePluginName("bbb a"), "bbb a")
-	assert.False(t, validatePluginName("cccd="), "cccd=")
-	assert.False(t, validatePluginName("reearth"), "reearth")
-	assert.False(t, validatePluginName(strings.Repeat("a", 101)), "over 100 chars")
+	t.Parallel()
+	testCases := []struct {
+		name     string
+		input    string
+		expected bool
+	}{
+		{
+			name:     "accepted name",
+			input:    "1cc.1_c-d",
+			expected: true,
+		},
+		{
+			name:     "les then 100",
+			input:    strings.Repeat("a", 100),
+			expected: true,
+		},
+		{
+			name:     "empty",
+			input:    "",
+			expected: false,
+		},
+		{
+			name:     "spaces",
+			input:    "    ",
+			expected: false,
+		},
+		{
+			name:     "contains not accepted characters",
+			input:    "@bbb/aa-a_a",
+			expected: false,
+		},
+		{
+			name:     "contain space",
+			input:    "bbb a",
+			expected: false,
+		},
+		{
+			name:     "contain =",
+			input:    "cccd=",
+			expected: false,
+		},
+		{
+			name:     "contains reearth reserved key word",
+			input:    "reearth",
+			expected: false,
+		},
+		{
+			name:     "more than 100 char",
+			input:    strings.Repeat("a", 101),
+			expected: false,
+		},
+	}
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(tt *testing.T) {
+			tt.Parallel()
+			assert.Equal(tt, tc.expected, validatePluginName(tc.input))
+		})
+	}
+}
+
+func TestPluginIDFrom(t *testing.T) {
+	t.Parallel()
+	testCases := []struct {
+		name     string
+		input    string
+		expected struct {
+			err    error
+			result PluginID
+		}
+	}{
+		{
+			name:  "accepted name",
+			input: "1cc.1_c-d#1.0.0",
+			expected: struct {
+				err    error
+				result PluginID
+			}{
+				err: nil,
+				result: PluginID{
+					name:    "1cc.1_c-d",
+					version: "1.0.0",
+					sys:     false,
+				},
+			},
+		},
+	}
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(tt *testing.T) {
+			tt.Parallel()
+			result, _ := PluginIDFrom(tc.input)
+			assert.Equal(tt, tc.expected.result, result)
+		})
+	}
 }
