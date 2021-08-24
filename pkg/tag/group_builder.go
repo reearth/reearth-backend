b5fcb4d66ec5235540e681d97dcd0ac3d6400646
mimoham24 <69579255+mimoham24@users.noreply.github.com>
2021-08-24 20:07:04 +0300

    feat: tag system domain models (#39)
    * feat: tag system domain models
    
    * refactor: * add tag interface * tag -> group and tag->item conversation
    
    * testing: generate test cases for the tagID
    
    * resolve notes
    
    * fix unit tests errors
    
    * add NewId test code
    fix NewId func
    
    * add more test cases
    refactor some parts

---
+++ pkg/tag/group_builder.go
@@ -0,0 +1,58 @@
+package tag
+
+import (
+	"errors"
+
+	"github.com/reearth/reearth-backend/pkg/id"
+)
+
+var ErrEmptyTagList = errors.New("tag list can't be empty")
+
+type GroupBuilder struct {
+	g *Group
+}
+
+func NewGroup() *GroupBuilder {
+	return &GroupBuilder{g: &Group{}}
+}
+
+func (b *GroupBuilder) Build() (*Group, error) {
+	if id.ID(b.g.id).IsNil() {
+		return nil, id.ErrInvalidID
+	}
+	if id.ID(b.g.sceneId).IsNil() {
+		return nil, ErrInvalidSceneID
+	}
+	if b.g.label == "" {
+		return nil, ErrEmptyLabel
+	}
+	if len(b.g.tags.tags) == 0 {
+		return nil, ErrEmptyTagList
+	}
+	return b.g, nil
+}
+
+func (b *GroupBuilder) ID(tid id.TagID) *GroupBuilder {
+	b.g.id = tid
+	return b
+}
+
+func (b *GroupBuilder) NewID() *GroupBuilder {
+	b.g.id = id.NewTagID()
+	return b
+}
+
+func (b *GroupBuilder) Label(l string) *GroupBuilder {
+	b.g.label = l
+	return b
+}
+
+func (b *GroupBuilder) Scene(sid id.SceneID) *GroupBuilder {
+	b.g.sceneId = sid
+	return b
+}
+
+func (b *GroupBuilder) Tags(tl List) *GroupBuilder {
+	b.g.tags = tl
+	return b
+}
