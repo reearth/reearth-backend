c5d7ab7da3a5557722264cccb39d2e18ffd57f0f
mimoham24 <69579255+mimoham24@users.noreply.github.com>
2021-09-06 11:00:44 +0300

    feat: tag system data-layer (mongo) (#44)
    * feat: tag system data-layer (mongo)
    
    * remove len > 0 check
    
    * goimport
    
    * Update pkg/tag/group_builder.go
    
    Co-authored-by: rot1024 <aayhrot@gmail.com>
    
    * Update pkg/tag/item_builder.go
    
    Co-authored-by: rot1024 <aayhrot@gmail.com>
    
    * rename itemFrom and groupFrom funcs
    
    Co-authored-by: rot1024 <aayhrot@gmail.com>

---
+++ internal/usecase/repo/tag.go
@@ -0,0 +1,23 @@
+package repo
+
+import (
+	"context"
+
+	"github.com/reearth/reearth-backend/pkg/id"
+	"github.com/reearth/reearth-backend/pkg/tag"
+)
+
+type Tag interface {
+	FindByID(context.Context, id.TagID, []id.SceneID) (*tag.Tag, error)
+	FindByIDs(context.Context, []id.TagID, []id.SceneID) ([]*tag.Tag, error)
+	FindItemByID(context.Context, id.TagID, []id.SceneID) (*tag.Item, error)
+	FindItemByIDs(context.Context, []id.TagID, []id.SceneID) ([]*tag.Item, error)
+	FindGroupByID(context.Context, id.TagID, []id.SceneID) (*tag.Group, error)
+	FindGroupByIDs(context.Context, []id.TagID, []id.SceneID) ([]*tag.Group, error)
+	FindByScene(context.Context, id.SceneID) ([]*tag.Tag, error)
+	Save(context.Context, tag.Tag) error
+	SaveAll(context.Context, []*tag.Tag) error
+	Remove(context.Context, id.TagID) error
+	RemoveAll(context.Context, []id.TagID) error
+	RemoveByScene(context.Context, id.SceneID) error
+}
