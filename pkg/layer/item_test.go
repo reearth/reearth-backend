10467b07e227ca3e8b751b87d21c8fed82980bdd
rot1024 <aayhrot@gmail.com>
2021-12-17 12:56:50 +0900

    fix

--- pkg/layer/item_test.go
+++ pkg/layer/item_test.go
@@ -1,23 +1,3 @@
 package layer
 
-import (
-	"github.com/reearth/reearth-backend/pkg/id"
-)
-
 var _ Layer = &Item{}
-
-var tags2 = []id.TagID{id.NewTagID()}
-var item = Item{
-	layerBase: layerBase{
-		id:        id.MustLayerID(id.New().String()),
-		name:      "xxx",
-		visible:   false,
-		plugin:    id.MustPluginID("aaa~1.1.1").Ref(),
-		extension: id.PluginExtensionID("foo").Ref(),
-		property:  nil,
-		infobox:   nil,
-		tags:      nil,
-		scene:     id.SceneID{},
-	},
-	linkedDataset: nil,
-}
