9a22761e3fdc3640045b85c699f50bc2d4b77418
rot1024 <aayhrot@gmail.com>
2022-02-10 15:44:29 +0900

    enhance scene/Plugins.Upgrade

--- pkg/scene/sceneops/plugin_migrator.go
+++ pkg/scene/sceneops/plugin_migrator.go
@@ -100,7 +100,7 @@ func (s *PluginMigrator) MigratePlugins(ctx context.Context, sc *scene.Scene, ol
 	}
 
 	// シーンのプラグイン
-	sc.Plugins().Upgrade(oldPluginID, newPluginID)
+	sc.Plugins().Upgrade(oldPluginID, newPluginID, nil, false)
 	for _, sp := range sc.Plugins().Plugins() {
 		if sp.Plugin().Equal(newPluginID) && sp.Property() != nil {
 			propertyIDs = append(propertyIDs, *sp.Property())
