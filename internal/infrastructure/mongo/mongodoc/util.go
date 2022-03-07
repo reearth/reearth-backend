c73241916bf6e3090f2c764f3d3bd71f7a1570c4
yk <yk.eukarya@gmail.com>
2022-03-07 11:24:42 +0300

    - remove unused code

--- internal/infrastructure/mongo/mongodoc/util.go
+++ internal/infrastructure/mongo/mongodoc/util.go
@@ -18,19 +18,3 @@ func convertDToM(i interface{}) interface{} {
 	}
 	return i
 }
-
-func appendE(f interface{}, elements ...bson.E) interface{} {
-	switch f2 := f.(type) {
-	case bson.D:
-		for _, e := range elements {
-			f2 = append(f2, e)
-		}
-		return f2
-	case bson.M:
-		for _, e := range elements {
-			f2[e.Key] = e.Value
-		}
-		return f2
-	}
-	return f
-}
