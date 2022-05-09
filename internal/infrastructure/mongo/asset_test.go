7810ec8f93ace735bd7596837106a4164dd3eec4
KeisukeYamashita <19yamashita15@gmail.com>
2022-05-09 23:06:33 +0900

    chore: refactor common db conn & cleanup test code for mongo
    Signed-off-by: KeisukeYamashita <19yamashita15@gmail.com>

--- internal/infrastructure/mongo/asset_test.go
+++ internal/infrastructure/mongo/asset_test.go
@@ -2,27 +2,15 @@ package mongo
 
 import (
 	"context"
-	"os"
 	"testing"
 	"time"
 
-	"github.com/reearth/reearth-backend/internal/infrastructure/mongo/mongodoc"
 	"github.com/reearth/reearth-backend/pkg/asset"
 	"github.com/reearth/reearth-backend/pkg/id"
 	"github.com/stretchr/testify/assert"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/x/mongo/driver/uuid"
 )
 
 func TestFindByID(t *testing.T) {
-	// Skip unit testing if "REEARTH_DB" is not configured
-	// See details: https://github.com/reearth/reearth/issues/273
-	db := os.Getenv("REEARTH_DB")
-	if db == "" {
-		return
-	}
-
 	tests := []struct {
 		Name     string
 		Expected struct {
@@ -48,12 +36,7 @@ func TestFindByID(t *testing.T) {
 		},
 	}
 
-	c, _ := mongo.Connect(
-		context.Background(),
-		options.Client().
-			ApplyURI(db).
-			SetConnectTimeout(time.Second*10),
-	)
+	initDB := connect(t)
 
 	for _, tc := range tests {
 		tc := tc
@@ -61,17 +44,13 @@ func TestFindByID(t *testing.T) {
 		t.Run(tc.Name, func(t *testing.T) {
 			t.Parallel()
 
-			database, _ := uuid.New()
-			client := mongodoc.NewClient(string(database[:]), c)
-			repo := NewAsset(client)
+			client, dropDB := initDB()
+			defer dropDB()
 
+			repo := NewAsset(client)
 			ctx := context.Background()
 			err := repo.Save(ctx, tc.Expected.Asset)
 			assert.NoError(t, err)
-
-			defer func() {
-				_ = c.Database(string(database[:])).Drop(ctx)
-			}()
 
 			got, err := repo.FindByID(ctx, tc.Expected.Asset.ID())
 			assert.NoError(t, err)
