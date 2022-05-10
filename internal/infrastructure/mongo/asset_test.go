19065099cbaf156139e1e08df3db5c4a8b4270f4
KeisukeYamashita <19yamashita15@gmail.com>
2022-05-10 23:14:26 +0900

    test: add Mongo Asset's Remove unit testing
    Signed-off-by: KeisukeYamashita <19yamashita15@gmail.com>

--- internal/infrastructure/mongo/asset_test.go
+++ internal/infrastructure/mongo/asset_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/reearth/reearth-backend/pkg/asset"
 	"github.com/reearth/reearth-backend/pkg/id"
+	"github.com/reearth/reearth-backend/pkg/rerror"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -61,6 +62,58 @@ func TestFindByID(t *testing.T) {
 			assert.Equal(t, tc.Expected.Asset.Size(), got.Size())
 			assert.Equal(t, tc.Expected.Asset.Name(), got.Name())
 			assert.Equal(t, tc.Expected.Asset.ContentType(), got.ContentType())
+		})
+	}
+}
+
+func TestRemove(t *testing.T) {
+	tests := []struct {
+		Name     string
+		Expected struct {
+			Name  string
+			Asset *asset.Asset
+		}
+	}{
+		{
+			Expected: struct {
+				Name  string
+				Asset *asset.Asset
+			}{
+				Asset: asset.New().
+					NewID().
+					CreatedAt(time.Now()).
+					Team(id.NewTeamID()).
+					Name("name").
+					Size(10).
+					URL("hxxps://https://reearth.io/").
+					ContentType("json").
+					MustBuild(),
+			},
+		},
+	}
+
+	initDB := connect(t)
+
+	for _, tc := range tests {
+		tc := tc
+
+		t.Run(tc.Name, func(t *testing.T) {
+			t.Parallel()
+
+			client, dropDB := initDB()
+			defer dropDB()
+
+			repo := NewAsset(client)
+			ctx := context.Background()
+			err := repo.Save(ctx, tc.Expected.Asset)
+			assert.NoError(t, err)
+
+			err = repo.Remove(ctx, tc.Expected.Asset.ID())
+			assert.NoError(t, err)
+
+			got, err := repo.FindByID(ctx, tc.Expected.Asset.ID())
+			assert.Equal(t, err, rerror.ErrNotFound)
+			assert.Nil(t, got)
 		})
 	}
 }
